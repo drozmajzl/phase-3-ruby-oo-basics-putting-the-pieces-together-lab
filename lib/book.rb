2a07b660782c2b017e11af5a6a567c28efc404de
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-02-24 17:02:03 -0500

    commit tree 52f4c413c26276db25a7793d046aa1d0bace7197

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,28 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_reader :turn_page
 
+    def initialize(title)
+        @title=title
+    end
+
+    def author=(author)
+        @author=author
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    attr_accessor :cobble
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        if @condition == "old"
+            @condition = "new"
+        end
+        puts "Your shoe is as good as new!"
+    end
+   
+end
